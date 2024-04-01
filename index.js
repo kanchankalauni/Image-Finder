718921fccf3b667e0d1bbbaeaa8e6f53da737aff
Kanchan Kalauni <kanchankalauni03@gmail.com>
2024-04-02 03:05:19 +0530

    update

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const access_token = 'ddmkwvwDZmiuQ2F9oh3pm96QMqmHJhCuGxfWzAVa9SM'
 let searchText = document.getElementById('searchText')
 let cardContainer = document.getElementById('cardContainer')
-
+let loadMoreBtn = document.getElementById('loadMoreBtn')
+let clearAllBtn = document.getElementById('clearAllBtn')
+let val;
+let page = 1;
 
 document.getElementById('searchBtn').addEventListener('click', () => {
+    page = 1;
     cardContainer.innerHTML = ''
-    let val = searchText.value
+    val = searchText.value
     searchText.value = ''
     fetchImg(val)
 })
 
 async function fetchImg(val){
-    let response = await fetch(`https://api.unsplash.com/search/photos?query=${val}&client_id=${access_token}`)
+    let response = await fetch(`https://api.unsplash.com/search/photos?query=${val}&client_id=${access_token}&page=${page}`)
     let result = await response.json()
-    console.log(result)
-    displayImg(result)
+    if (result.results.length < 1) {
+        cardContainer.innerHTML = `<h1 class="errorHeading">Not Found</h1>`
+    } else {
+        displayImg(result)
+    }
 }
 
 function displayImg(res){
@@ -32,4 +39,19 @@ function displayImg(res){
         </div>`
         cardContainer.appendChild(div)
     })
+
+    loadMoreBtn.classList.add('visible')
+    clearAllBtn.classList.add('visible')
 }
+
+loadMoreBtn.addEventListener('click', () => {
+    // console.log("first")
+    page++;
+    fetchImg(val)
+})
+
+clearAllBtn.addEventListener('click', () => {
+    loadMoreBtn.classList.add('hidden')
+    clearAllBtn.classList.add('hidden')
+    cardContainer.innerHTML = ''
+})
